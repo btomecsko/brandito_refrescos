51cb152d45ce2d43c5f49c78a5b91b07b0724b69
btomecsko <brandon.tomecsko92@gmail.com>
2022-06-08 15:45:22 -0500

    Testing various methods to render text

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //HTML ids to variables
 
+const mainBody = () => document.getElementById('main_body')
 const menu = () => document.getElementById('menu');
 const checkout = () => document.getElementById('checkout');
 
-//DOM Render functions
+//DOM Render functions variable
+
 function renderMenuItem(menuItem){
     //build menu item card
     let card = document.createElement("li")
@@ -14,30 +16,44 @@ function renderMenuItem(menuItem){
           <h3>${menuItem.name}</h3>
           <p>${menuItem.description}</p>
         </div>
+        <div>
+        <button>Order: ${menuItem.cost}</button>
+        </div>
     `
     document.querySelector('#menu_items').appendChild(card)
 }
 
 
 //Initial Render
+
 function initialize(){
     menuData.filter(menuItem => renderMenuItem(menuItem))
 }
 
 initialize()
 
+
 //Event handlers
 
 const renderMenu = (e) => {
     e.preventDefault();
 
-    alert ('hi');
+    //removing the main body or about when menu is clicked
+    resetMainBody();
+
+    //adding render function to populate the menu items
+    
+    
 }
 
 //Event listeners
 
 const menuEvent = () => {
     menu().addEventListener('click', renderMenu)
+}
+
+const resetMainBody = () => {
+    mainBody().innerHTML = "";
 }
 
 document.addEventListener('DOMContentLoaded', () => {
